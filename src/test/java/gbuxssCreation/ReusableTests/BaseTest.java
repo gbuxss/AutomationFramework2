49fe66eefb30181efbfab6d2c929927dcdbed3e5
gbuxss <jeevandhamala095@gmail.com>
2022-10-04 11:49:53 -0500

    Update BaseTest.java
    commented headless

--- src/test/java/gbuxssCreation/ReusableTests/BaseTest.java
+++ src/test/java/gbuxssCreation/ReusableTests/BaseTest.java
@@ -41,7 +41,7 @@ public class BaseTest {
 			ChromeOptions co = new ChromeOptions();
 			co.addArguments("--no-sandbox");
 			co.addArguments("--disable-dev-shm-usage");
-			co.addArguments("headless");
+			//co.addArguments("headless");
 			driver = new ChromeDriver(co);
 
 
